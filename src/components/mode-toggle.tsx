f53a237dd65a7fb481c7f32940c0261f1ad4ba8a
Ahmed Metwaly <luisarabss@gmail.com>
2025-07-10 14:57:41 +0000

    دارك مود و لايت مود و وضع وسط لإراحة العين

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Eye } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -13,7 +13,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
     <DropdownMenu>
@@ -21,6 +21,7 @@ export function ModeToggle() {
         <Button variant="ghost" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Eye className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all neutral:rotate-0 neutral:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
@@ -30,6 +31,9 @@ export function ModeToggle() {
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           داكن
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("neutral")}>
+          محايد
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
           النظام
